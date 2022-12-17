c4095852bfe135ea6291a75680a8189fa85df5c2
emecii <emecii23@gmail.com>
2022-12-16 21:46:32 -0800

    add dotenv

--- espresso-backend-services/app.py
+++ espresso-backend-services/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify
 from model import Model
 import os
+from flask import send_from_directory
+from dotenv import load_dotenv
 
 
 
 app = Flask(__name__)
+
+load_dotenv('.env')
 
 # MongoDB connection string, stored in a secret in GitHub
 connection_string = os.environ['MONGO_CONNECTION_STRING']
@@ -27,6 +31,14 @@ def get_model(model_id):
 
     # Return the model as JSON
     return jsonify(model)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+  if path != "" and os.path.exists(app.static_folder + '/' + path):
+    return send_from_directory(app.static_folder, path)
+  else:
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run()
