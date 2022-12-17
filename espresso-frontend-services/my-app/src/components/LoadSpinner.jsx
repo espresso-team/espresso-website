59a6b1a4a36b42ecf8208fae5a3bde7074e3c814
Wangyuan <zengwangyuan@outlook.com>
2022-12-16 20:35:30 -0800

    Change the app to TS

--- espresso-frontend-services/my-app/src/components/LoadSpinner.jsx
+++
@@ -1,7 +0,0 @@
-const LoadSpinner = () => {
-    return (
-        <h1>LoadSpinner</h1>
-    )
-}
-
-export default LoadSpinner;
--- espresso-frontend-services/my-app/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LoadSpinner } from './LoadSpinner';
-export { default as Navbar } from './Navbar';
-export { default as Welcome } from './Welcome';
