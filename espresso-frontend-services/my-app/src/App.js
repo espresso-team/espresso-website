31655207ca91e395c2f95f191fed3ace41781fac
surpasslll <surpasslza@gmail.com>
2022-12-16 19:44:27 -0800

    front end setup

---
+++ espresso-frontend-services/my-app/src/App.js
@@ -0,0 +1,14 @@
+import { Navbar, Welcome } from './components'
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+    </div>
+  )
+}
+
+export default App;
