515c9ab6ef59dff9fae9836712b4ec5a51ed029d
emecii <emecii23@gmail.com>
2023-03-07 21:24:42 -0800

    fix jump out and support prompt larger than MAX_TOKEN

--- espresso-backend-services/chat-gpt-backend/util.js
+++ espresso-backend-services/chat-gpt-backend/util.js
@@ -5,3 +5,11 @@ export function build_chat_history_for_frontend(chat_history) {
   });
   return chat_history_for_frontend;
 }
+
+export function is_response_include_forbidden_words(res_message) {
+    if (res_message.includes('ChatGPT') || res_message.includes('AI') ||
+        res_message.includes('语言模型') || res_message.includes('很抱歉') || 
+        res_message.includes('聊天机器人')) {
+            return true;
+        }
+}
