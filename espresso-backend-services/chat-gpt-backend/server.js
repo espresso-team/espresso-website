37340a3c38dc8cb6f362a4ae459d854aac2aec4a
emecii <emecii23@gmail.com>
2023-05-01 23:28:14 -0700

    bug fix

--- espresso-backend-services/chat-gpt-backend/server.js
+++ espresso-backend-services/chat-gpt-backend/server.js
@@ -100,60 +100,63 @@ app.post("/send-message", async (req, res) => {
 
 // Route when a new user join chat
 app.post("/join-chat", async (req, res) => {
-  const user_id = req.body.user_id;
-  const model_id = req.body.model_id;
-  var user = await getUserByUserId(user_id);
-  var model = await getModelByModelId(model_id);
-  const model_gender = model.model_type;
-  var chat_client = new ChatClient(user.user_name, model.model_name);
-  // check if user has an existing conv
-  var cond = {
-    user_id: user_id,
-    model_id: model_id
-  }
-  var existing_conv = await getConv(cond);
-  if (existing_conv != null) {
-    // find an existing conv
-    var conv = existing_conv.conv_id;
-    var chat_history = await getChatHistoryByConvId(conv);
-    var return_chat_history = chat_history.slice(-10);
-    if (!return_chat_history) {
-      return_chat_history = [];
-    }
-    // console.log(chat_history);
-    const return_mes = return_greeting_words(model_gender);
-    // TODO: make insertChat and return a transaction
-    await chat_client.reinit_conv(conv, existing_conv.last_msg_id, model_id, chat_history);
-    await insertChat({ conv_id: conv, message: return_mes, is_user: false });
-    // get the last 10 chat history
-    res.json({
-      message: return_mes, status: "success",
-      user_id: user_id, model_id: model_id,
-      chat_history: return_chat_history
-    });
-    return;
-  }
-  // init a new converstation for a new user
-  var response = await chat_client.init_conv(model_id);
-  console.log(response);
-  // retry if response contains forbidden words
-  if (is_response_include_forbidden_words(response.response)) {
-    response = await chat_client.init_conv(model_id);
-  }
-
-  var msg = response.response;
-  // if still contains forbidden words, return a greeting message
-  if (is_response_include_forbidden_words(msg)) {
-    msg = return_greeting_words(model_gender);
-  }
-  var conv = {
-    user_id: user_id,
-    model_id: model_id,
-    conv_id: response.conversationId,
-    last_msg_id: response.messageId
-  };
-
   try {
+    const user_id = req.body.user_id;
+    const model_id = req.body.model_id;
+    var user = await getUserByUserId(user_id);
+    var model = await getModelByModelId(model_id);
+    const model_gender = model.model_type;
+    var user_name = "用户";
+    if (user && user.user_name) {
+      user_name = user.user_name;
+    }
+    var chat_client = new ChatClient(user_name, model.model_name);
+    // check if user has an existing conv
+    var cond = {
+      user_id: user_id,
+      model_id: model_id
+    }
+    var existing_conv = await getConv(cond);
+    if (existing_conv != null) {
+      // find an existing conv
+      var conv = existing_conv.conv_id;
+      var chat_history = await getChatHistoryByConvId(conv);
+      var return_chat_history = chat_history.slice(-10);
+      if (!return_chat_history) {
+        return_chat_history = [];
+      }
+      // console.log(chat_history);
+      const return_mes = return_greeting_words(model_gender);
+      // TODO: make insertChat and return a transaction
+      await chat_client.reinit_conv(conv, existing_conv.last_msg_id, model_id, chat_history);
+      await insertChat({ conv_id: conv, message: return_mes, is_user: false });
+      // get the last 10 chat history
+      res.json({
+        message: return_mes, status: "success",
+        user_id: user_id, model_id: model_id,
+        chat_history: return_chat_history
+      });
+      return;
+    }
+    // init a new converstation for a new user
+    var response = await chat_client.init_conv(model_id);
+    console.log(response);
+    // retry if response contains forbidden words
+    if (is_response_include_forbidden_words(response.response)) {
+      response = await chat_client.init_conv(model_id);
+    }
+
+    var msg = response.response;
+    // if still contains forbidden words, return a greeting message
+    if (is_response_include_forbidden_words(msg)) {
+      msg = return_greeting_words(model_gender);
+    }
+    var conv = {
+      user_id: user_id,
+      model_id: model_id,
+      conv_id: response.conversationId,
+      last_msg_id: response.messageId
+    };
     await createConversation(conv);
     await insertChat({ conv_id: response.conversationId, message: msg, is_user: false });
     res.json({
