e9af43395295ebbd421b708e46bb9b968186514e
emecii <emecii23@gmail.com>
2023-05-23 22:20:13 -0700

    support chichat-ai.com

--- espresso-backend-services/chat-gpt-backend/server.js
+++ espresso-backend-services/chat-gpt-backend/server.js
@@ -21,6 +21,7 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 const app = express();
 const allowedOrigins = [
+  'https://www.chitchat-ai.com/',
   'https://chitchat-ai-mm27.onrender.com',
   'https://chitchat-ai-dev.onrender.com',
   'http://localhost:3001',
