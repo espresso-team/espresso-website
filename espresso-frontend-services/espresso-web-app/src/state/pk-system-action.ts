1297414689b2ae48b88325d3f0b659f59b3227c5
emecii <emecii23@gmail.com>
2023-06-11 17:31:09 -0700

    fix chat history avatar

--- espresso-frontend-services/espresso-web-app/src/state/pk-system-action.ts
+++ espresso-frontend-services/espresso-web-app/src/state/pk-system-action.ts
@@ -50,10 +50,10 @@ export const pkSystemAction = {
                     if (err.response && err.response.status === 409) {
                         // handle conflict error here
                         console.log('User profile already exists for this user_id');
-                      } else {
+                    } else {
                         message.error("页面错误，请刷新重试");
                         console.error(err);
-                      }
+                    }
                 });
 
         },
@@ -132,12 +132,12 @@ export const pkSystemAction = {
                 setState({ curImageId: newImageId });
             },
     isModelSelected:
-            () =>
-                ({ getState }: pkSystemApi) => {
-                    if(getState().curModelName === 'AI角色')
-                        return false;
-                    return true;
-                },
+        () =>
+            ({ getState }: pkSystemApi) => {
+                if (getState().curModelName === 'AI角色')
+                    return false;
+                return true;
+            },
     cleanMessageList:
         () =>
             ({ setState }: pkSystemApi) => {
@@ -159,95 +159,95 @@ export const pkSystemAction = {
                 setState({ messageList: newMessageList });
             },
     handleJoinChat:
-    (modelId: string) =>
+        (modelId: string) =>
             async ({ getState, setState }: pkSystemApi) => {
-        let modelName: string = "AI角色";
-        let modelSrc: string = "";
-        console.log("pkSystemAction- handleJoinChat", modelId);
-        // Fetch modelName and modelSrc from backend
-        await axios
-        .get(`${ENDPOINT}/model-profile`,
-          {
-            params: {
-              model_id: modelId
-            }
-          })
-        .then((response) => {
-          console.log("handleJoinChat - fetchModelProfile", response)
-          if (response.status === HttpStatus.OK) {
-            const curModelArray = response.data.data as Model[];
-            console.log("handleJoinChat - curModelArray", curModelArray)
-            modelName = curModelArray[0].model_name;
-            modelSrc = curModelArray[0].model_metadata.image_url;
-          }
-          else {
-            message.error("获取角色信息失败，请刷新重试")
-            console.log("handleJoinChat - fetchModelProfile response failed", response)
-          }
-        })
-        .catch((err) => {
-          message.error("获取角色信息失败，请刷新重试");
-          console.log("handleJoinChat - fetchModelProfile Error", err)
-        });
-        // update curImage id in state since we will use it on send message post, and convert it to string
-        //setState({curImageId: +modelId});
-        setState({curModelIdString: modelId});
-        // update curModel name and src
-        setState({curModelName: modelName});
-        setState({curModelSrc: modelSrc});
-        await axios
-          .post(`${ENDPOINT}/join-chat`,
-            {
-              "user_id": getState().userId,
-              "model_id": modelId
-            }
-          )
-          .then((response) => {
-            setState({ messageList: [] });
-            let chatHistory:ChatHistoryItem[] = [];
-            if(response.data.chat_history) {
-             chatHistory = response.data.chat_history;
-            }
-            console.log("handleJoinChat - chatHistory", chatHistory)
-            const message = response.data.message;
-            const uID = response.data.user_id;
-            const mID = response.data.model_id;
+                let modelName: string = "AI角色";
+                let modelSrc: string = "";
+                console.log("pkSystemAction- handleJoinChat", modelId);
+                // Fetch modelName and modelSrc from backend
+                await axios
+                    .get(`${ENDPOINT}/model-profile`,
+                        {
+                            params: {
+                                model_id: modelId
+                            }
+                        })
+                    .then((response) => {
+                        console.log("handleJoinChat - fetchModelProfile", response)
+                        if (response.status === HttpStatus.OK) {
+                            const curModelArray = response.data.data as Model[];
+                            console.log("handleJoinChat - curModelArray", curModelArray)
+                            modelName = curModelArray[0].model_name;
+                            modelSrc = curModelArray[0].model_metadata.image_url;
+                        }
+                        else {
+                            message.error("获取角色信息失败，请刷新重试")
+                            console.log("handleJoinChat - fetchModelProfile response failed", response)
+                        }
+                    })
+                    .catch((err) => {
+                        message.error("获取角色信息失败，请刷新重试");
+                        console.log("handleJoinChat - fetchModelProfile Error", err)
+                    });
+                // update curImage id in state since we will use it on send message post, and convert it to string
+                //setState({curImageId: +modelId});
+                setState({ curModelIdString: modelId });
+                // update curModel name and src
+                setState({ curModelName: modelName });
+                setState({ curModelSrc: modelSrc });
+                await axios
+                    .post(`${ENDPOINT}/join-chat`,
+                        {
+                            "user_id": getState().userId,
+                            "model_id": modelId
+                        }
+                    )
+                    .then((response) => {
+                        setState({ messageList: [] });
+                        let chatHistory: ChatHistoryItem[] = [];
+                        if (response.data.chat_history) {
+                            chatHistory = response.data.chat_history;
+                        }
+                        console.log("handleJoinChat - chatHistory", chatHistory)
+                        const message = response.data.message;
+                        const uID = response.data.user_id;
+                        const mID = response.data.model_id;
 
-            // Add chat history to messageList
-            chatHistory.map(chat => {
-              const isUser:boolean = chat.is_user;
-              const messageItem = {
-                "text": chat.message,
-                "id": mID,
-                "sender": {
-                  "name": isUser ? getState().curUserName : modelName,
-                  "uid": uID,
-                  "avatar": isUser ? "https://data.cometchat.com/assets/images/avatars/ironman.png" : modelSrc,
-                }
-              } as IMessage;
-              //pkSystemAction.updateMessageList(messageItem);
-              const newMessageList: IMessage[] = JSON.parse(JSON.stringify(getState().messageList));
-              newMessageList.push(messageItem);
-              setState({ messageList: newMessageList });
-            })
+                        // Add chat history to messageList
+                        chatHistory.map(chat => {
+                            const isUser: boolean = chat.is_user;
+                            const messageItem = {
+                                "text": chat.message,
+                                "id": mID,
+                                "sender": {
+                                    "name": isUser ? getState().curUserName : modelName,
+                                    "uid": uID,
+                                    "avatar": isUser ? (getState().userGender === GenderType.FAMALE ? "https://chichat-images-1317940514.cos.ap-nanjing.myqcloud.com/static/WechatIMG4576.jpg" : "https://chichat-images-1317940514.cos.ap-nanjing.myqcloud.com/static/WechatIMG4577.jpg") : modelSrc,
+                                }
+                            } as IMessage;
+                            //pkSystemAction.updateMessageList(messageItem);
+                            const newMessageList: IMessage[] = JSON.parse(JSON.stringify(getState().messageList));
+                            newMessageList.push(messageItem);
+                            setState({ messageList: newMessageList });
+                        })
 
-            const initialMessage =
-              {
-                "text": message,
-                "id": mID,
-                "sender": {
-                  "name": modelName,
-                  "uid": uID,
-                  "avatar": modelSrc,
-                }
-              } as IMessage;
-              //pkSystemAction.updateMessageList(initialMessage);
-              const newMessageList: IMessage[] = JSON.parse(JSON.stringify(getState().messageList));
-              newMessageList.push(initialMessage);
-              setState({ messageList: newMessageList });
-          })
-          .catch((err) => console.log(err));
-      }
+                        const initialMessage =
+                            {
+                                "text": message,
+                                "id": mID,
+                                "sender": {
+                                    "name": modelName,
+                                    "uid": uID,
+                                    "avatar": modelSrc,
+                                }
+                            } as IMessage;
+                        //pkSystemAction.updateMessageList(initialMessage);
+                        const newMessageList: IMessage[] = JSON.parse(JSON.stringify(getState().messageList));
+                        newMessageList.push(initialMessage);
+                        setState({ messageList: newMessageList });
+                    })
+                    .catch((err) => console.log(err));
+            }
 };
 
 export type PkSystemAction = typeof pkSystemAction;
