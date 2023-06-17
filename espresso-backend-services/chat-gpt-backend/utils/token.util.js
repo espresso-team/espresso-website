3347fca51a9d57a0f9c52545e44f34ada047bdcc
emecii <emecii23@gmail.com>
2023-06-16 22:09:00 -0700

    temporarily set token expiration time to be 3h

--- espresso-backend-services/chat-gpt-backend/utils/token.util.js
+++ espresso-backend-services/chat-gpt-backend/utils/token.util.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 export function createJwtToken(payload) {
-  const token = sign(payload, JWT_SECRET, { expiresIn: "7 days" });
+  const token = sign(payload, JWT_SECRET, { expiresIn: "3h" });
   return token;
 }
 
